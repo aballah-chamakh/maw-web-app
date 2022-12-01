713bf0045f87644479b13d8ca96d7d655d51203c
unknown <chamakhabdallah8@gmail.com>
2022-12-01 11:08:31 +0100

    new commit

--- backend/maw/WebApi/models.py
+++ backend/maw/WebApi/models.py
@@ -24,6 +24,27 @@ class LoxboxMonitorOrder(models.Model):
     transaction_id = models.CharField(max_length=255)
     state = models.CharField(max_length=50)
 
+# LOXBOX AREAS MODELS 
+
+class LoxboxCities(models.Model):
+    selected_all = models.BooleanField(default=False)
+
+class City(models.Model):
+    loxbox_cities = models.ForeignKey(LoxboxCities,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    selected_all = models.BooleanField(default=False)
+
+class Delegation(models.Model):
+    city = models.ForeignKey(City,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    selected_all = models.BooleanField(default=False)
+
+class Locality(models.Model):
+    delegation = models.ForeignKey(Delegation,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    selected = models.BooleanField(default=False)
+
+
     
 
 
